d533a61202d4c55a14ac38510fa86618ea008deb
Shibili Aman TK <dlshacker159@gmail.com>
2026-02-01 01:34:02 +0530

    added PassMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject container;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!container.activeSelf)
+            {
+                PauseButton();
+            }
+            else
+            {
+                ResumeButton();
+            }
+        }
+    }
+    public void PauseButton()
+    {
+        container.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeButton()
+    {
+        container.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void RestartButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitMenu()
+    {
+        Time.timeScale = 1;
+        if (EventSystem.current != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+        SceneManager.LoadScene(0);
+    }
+}
